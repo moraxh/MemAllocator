718b8f611a4c0c17da02e7bef91b04ae7b8b2496
Jorge Mora <jorgemorah671@gmail.com>
2023-02-10 13:01:56 -0600

    Delete a node from free list, Best Fit Search(Need to testing)

--- brk/src/alloc.c
+++ brk/src/alloc.c
@@ -1,5 +1,9 @@
 #include "alloc.h"
 
+// TODO Merge free blocks
+// Split free blocks
+// Test recent changes
+
 // Since sbrk() can only increase or decrease
 // its position we need a form to deallocate
 // blocks that are between allocated blocks
@@ -14,8 +18,30 @@ typedef struct Header{
 
 } Header;
 
+typedef struct FreeList {
+    Header * next;
+} FreeList;
+
 // Linked list of free blocks
 Header * head;
+
+// Binary operations
+void setUsed(Header * block, int used);
+size_t getSize(Header * block);
+int isUsed(Header * block);
+
+size_t align(size_t size);
+
+// Allocator
+Header * requestMemory(size_t size);
+void * alloc(size_t size);
+
+// Deallocator
+Header * bestFitSearch(size_t size);
+FreeList * getNodeFreeList(Header * block);
+void appendToFreeList(Header * block, FreeList * b_node);
+void removeFromFreeList(Header * block);
+void dealloc(void * addr);
 
 // We will use last digit of a size in binary to store if its used or not
 // Ex.
@@ -97,9 +123,10 @@ size_t align(size_t size) {
     }
 }
 
+
 // ----------------------------------------------------------------
 // Allocs a space of given memory
-Header * request_memory(size_t size) {
+Header * requestMemory(size_t size) {
     // Get current memory direction
     Header * addr = (Header *)sbrk(0);
 
@@ -115,20 +142,22 @@ Header * request_memory(size_t size) {
 void * alloc(size_t size) {
     // There is free blocks?
     if (head != NULL) {
-        // Seach throug linked list
+        Header * block = bestFitSearch(size);
+        if (block != NULL) {
+            return block + sizeof(Header);
+        }
     }
 
-    Header * block = request_memory(size);
+    Header * block = requestMemory(size);
     block->header = align(size);
     setUsed(block, 1);
 
     return ((void *)block + sizeof(Header));
 }
 
+
 // Deallocates a block from a given pointer
-typedef struct FreeList {
-    Header * next;
-} FreeList;
+// -------------------------------------------------------------------------------
 
 void printFreeList(Header * head) {
     if (head == NULL) {
@@ -138,7 +167,7 @@ void printFreeList(Header * head) {
 
     FreeList * s_node;
     do {
-        s_node = (FreeList *)((void*)head + sizeof(Header));
+        s_node = getNodeFreeList(head);
 
         printf("Head: %p\n", head);
         printf("FreeList: %p\n", s_node);
@@ -149,38 +178,17 @@ void printFreeList(Header * head) {
     printf("------------------------------------\n");
 }
 
-// To "delete" a reserved memory space we are gonna set
-// it state to unused, and add it to a free linked list,
-// this list will be stored inside the data block
-//
-//  (Header *)first
-//               |
-//               +------+            +-----------------+             +------------> NULL
-//                      |            |                 |             |
-//                 +----▼---+--------|---------+   +---▼----+--------|---------+
-//                 | Header | (FreeList *)Next |   | Header | (FreeList *)Next |
-//                 +--------+------------------+   +--------+------------------+
-//
-void dealloc(void * addr) {
-    // Set block parameters (unused) and create a node of the
-    // free linked list
-    Header * block = (Header *)(addr - sizeof(Header));
-    FreeList * b_node = (FreeList *)((void *)block + sizeof(Header));
-    b_node->next = NULL;
-    setUsed(block, 0);
+FreeList * getNodeFreeList(Header * block) {
+    return (FreeList *)((void *)block + sizeof(Header));
+}
 
-    // If FreeList is empty
-    if (head == NULL) {
-        head = block;
-        return;
-    }
-
-    // Append to the sorted list
+// Append to the sorted free list
+void appendToFreeList(Header * block, FreeList * b_node) {
     Header * s_header = head;
     FreeList * s_node = NULL;
 
     do {
-        s_node = (FreeList *)((void *)s_header + sizeof(Header));
+        s_node = getNodeFreeList(s_header);
 
         // Is block smaller than the first element?
         if (((void *)block < (void *)s_header) && s_header == head) {
@@ -212,4 +220,101 @@ void dealloc(void * addr) {
 
     // Insert block to the tail
     s_node->next = block;
+}
+
+// Remove a block from free list
+void removeFromFreeList(Header * block) {
+    // Free List only has one element
+    if(head == block && getNodeFreeList(block)->next == NULL) {
+        head = NULL;
+        return;
+    }
+
+    Header * s_node = head;
+    Header * prev_block = NULL;
+
+    do {
+        if(getNodeFreeList(s_node)->next == block) {
+            prev_block = s_node;
+        }
+        s_node = getNodeFreeList(s_node)->next;
+    }
+    while(s_node != NULL);
+
+    if (prev_block == NULL) { return; }
+
+    // If block is the tail of the linked list
+    if (getNodeFreeList(prev_block)->next == NULL) {
+        getNodeFreeList(prev_block)->next = NULL;
+        return;
+    }
+
+    // Link prev block to the next of the current block
+    //
+    //
+    //                                 +--------------------------------+ +---------------
+    //                                 |                                | |              |
+    // +-------------+-----------------|--+    +--------+---------------v-|--+    +------v------+--------------------+
+    // |  Prev Block | (FreeList *)->next |    |  Block | (FreeList *)->next |    |  Next Block | (FreeList *)->next |
+    // +-------------+--------------------+    +--------+--------------------+    +-------------+--------------------+
+    getNodeFreeList(prev_block)->next = getNodeFreeList(getNodeFreeList(prev_block)->next)->next;
+}
+
+Header * bestFitSearch(size_t size) {
+    if (head == NULL) { return NULL; }
+
+    Header * s_header = head;
+
+    // Try to search a block of the exact size
+    do {
+        if(size == getSize(s_header)) {
+            return s_header;
+        }
+
+        s_header = (Header *)getNodeFreeList(s_header)->next;
+    }
+    while(s_header != NULL);
+
+    // Try to search a bigger block
+    s_header = head;
+
+    do {
+        if(size < getSize(s_header)) {
+            return s_header;
+        }
+
+        s_header = (Header *)getNodeFreeList(s_header)->next;
+    }
+    while(s_header != NULL);
+
+    return NULL; // NULL means not found
+}
+
+// To "delete" a reserved memory space we are gonna set
+// it state to unused, and add it to a free linked list,
+// this list will be stored inside the data block
+//
+//  (Header *)first
+//               |
+//               +------+            +-----------------+             +------------> NULL
+//                      |            |                 |             |
+//                 +----v---+--------|---------+   +---v----+--------|---------+
+//                 | Header | (FreeList *)Next |   | Header | (FreeList *)Next |
+//                 +--------+------------------+   +--------+------------------+
+//
+void dealloc(void * addr) {
+    // Set block parameters (unused) and create a node of the
+    // free linked list
+    Header * block = (Header *)(addr - sizeof(Header));
+    FreeList * b_node = getNodeFreeList(block);
+    b_node->next = NULL;
+    setUsed(block, 0);
+
+    // If FreeList is empty
+    if (head == NULL) {
+        head = block;
+        return;
+    }
+
+    appendToFreeList(block, b_node);
 }
